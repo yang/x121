577dd3220c0ed1c80ff055d460894cb5f6bd6f18
Yang Zhang <yang@users.noreply.github.com>
2023-01-26 16:55:03 -0800

    CPA

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  // Turn off React StrictMode for now, as react-aria (used by Plasmic)
+  // has some troubles with it. See
+  // https://github.com/adobe/react-spectrum/labels/strict%20mode
+  reactStrictMode: false,
 }
 
 module.exports = nextConfig
+        
